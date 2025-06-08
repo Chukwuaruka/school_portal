801cc2e8886f368f2108c104650c696def809bd3
Chukwuaruka <vincentchukwuaruka123@gmail.com>
2025-06-08 17:59:10 +0100

    Update settings.py with AWS S3, email, and Render production settings

--- school_portal/settings.py
+++ school_portal/settings.py
@@ -114,14 +114,18 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # MEDIA FILES (AWS S3)
 # -------------------------------------------------
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_REGION_NAME = 'us-east-1'
-AWS_QUERYSTRING_AUTH = False
-AWS_DEFAULT_ACL = 'public-read'
+AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
-MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/"
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None  # Important for public-read issues
+AWS_QUERYSTRING_AUTH = False  # Ensures clean public URLs
+
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 
 # -------------------------------------------------
 # EMAIL (Gmail SMTP)
