2ab7191cf82f401d3a37e9a29e1a2a2288a5e5e3
Chukwuaruka <vincentchukwuaruka123@gmail.com>
2025-06-09 15:23:49 +0100

    Add S3 support for production media storage

--- school_portal/settings.py
+++ school_portal/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'portal',
+    'storages',  # <--- Added for S3
 ]
 
 # -------------------------------------------------
@@ -110,10 +111,24 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # -------------------------------------------------
-# MEDIA FILES (Local Storage)
+# MEDIA FILES (S3 in production, local in dev)
 # -------------------------------------------------
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+if not DEBUG:
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+    AWS_S3_REGION_NAME = 'us-east-1'  # e.g., 'us-east-1'
+    AWS_S3_FILE_OVERWRITE = False
+    AWS_DEFAULT_ACL = 'public-read'  # Or None if you want private files
+    AWS_QUERYSTRING_AUTH = False  # Prevents ?AWSAccessKeyId=... in URLs
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
+else:
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = BASE_DIR / 'media'
 
 # -------------------------------------------------
 # EMAIL (Gmail SMTP)
