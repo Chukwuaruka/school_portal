b05ebdf648deddcf674270d60a708ef6901e5015
Chukwuaruka <vincentchukwuaruka123@gmail.com>
2025-06-15 23:31:04 +0100

    Add updated settings.py

--- school_portal/settings.py
+++ school_portal/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'portal',
     'storages',  # For AWS S3
 ]
